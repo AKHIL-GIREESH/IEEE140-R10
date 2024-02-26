eb37b08d1c421a03f1d318ab6721730009a68997
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-02-26 17:37:21 +0530

    FAQ

--- ieee140-r10-frontend/components/FAQ.jsx
+++ ieee140-r10-frontend/components/FAQ.jsx
@@ -1,6 +1,29 @@
+import { FAQdata } from "../mocks/FAQ"
+
 const FAQ = () => {
     return(
-        <>FAQ</>
+        <>
+            <h1>For Quiries</h1>
+            <div className="glassWrapper-Query">
+                <input type="text" placeholder="Full Name" />
+                <input type="text" placeholder="Email" />
+                <input type="text" placeholder="Query" />
+                <button>Submit</button>
+            </div>
+
+            {
+                FAQdata.map(({id,Q,A}) => {
+                    return(
+                        <div className="glassWrapper-FAQ" key={id}>
+                            <p>{Q}</p>
+                            <p>{A}</p>
+                        </div>
+                    )
+                })
+            
+            }
+
+        </>
     )
 }
 
---
+++ ieee140-r10-frontend/mocks/FAQ.js
@@ -0,0 +1,21 @@
+export const FAQdata = [
+    {
+        Q:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar, urna nec sollicitudin accumsan, justo sem mollis lectus?",
+        A:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar, urna nec sollicitudin accumsan, justo sem mollis lectus, ut vehicula quam turpis eu elit. Donec nec velit id turpis fermentum bibendum vel in ex. Integer lacinia sit amet nunc a interdum. Nullam accumsan odio et augue molestie, a rhoncus tortor hendrerit. Vivamus vel sapien velit. ",
+        id:1  
+    },
+    {
+        Q:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar, urna nec sollicitudin accumsan, justo sem mollis lectus?",
+        A:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar, urna nec sollicitudin accumsan, justo sem mollis lectus, ut vehicula quam turpis eu elit. Donec nec velit id turpis fermentum bibendum vel in ex. Integer lacinia sit amet nunc a interdum. Nullam accumsan odio et augue molestie, a rhoncus tortor hendrerit. Vivamus vel sapien velit. ",
+        id:2  
+    },
+    {
+        Q:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar, urna nec sollicitudin accumsan, justo sem mollis lectus?",
+        A:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar, urna nec sollicitudin accumsan, justo sem mollis lectus, ut vehicula quam turpis eu elit. Donec nec velit id turpis fermentum bibendum vel in ex. Integer lacinia sit amet nunc a interdum. Nullam accumsan odio et augue molestie, a rhoncus tortor hendrerit. Vivamus vel sapien velit. ",
+        id:3  
+    },{
+        Q:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar, urna nec sollicitudin accumsan, justo sem mollis lectus?",
+        A:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar, urna nec sollicitudin accumsan, justo sem mollis lectus, ut vehicula quam turpis eu elit. Donec nec velit id turpis fermentum bibendum vel in ex. Integer lacinia sit amet nunc a interdum. Nullam accumsan odio et augue molestie, a rhoncus tortor hendrerit. Vivamus vel sapien velit. ",
+        id:4 
+    }
+]
