47e4ea5523798f3e22ff670ee9e73a226365145b
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-02-28 12:28:02 +0530

    Landing Pg

--- ieee140-r10-frontend/components/LandingPage/About.jsx
+++ ieee140-r10-frontend/components/LandingPage/About.jsx
@@ -1,8 +1,11 @@
 const About = () => {
     return(
-        <>
-            
-        </>
+        <div style={{height:"50vh",width:"90vw",border:"1px solid",marginLeft:"5vw"}}>
+            <div style={{width:"60%"}}>
+                <h1>About the Campaign</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at lacinia purus. Morbi ac ultricies ligula. Donec sed augue vel nisi vulputate fermentum. Phasellus rutrum sapien quis justo posuere, sed consequat eros rutrum. Sed nec magna consectetur, ultricies lectus id, efficitur neque. Aliquam erat volutpat. Sed ac lectus sit amet velit malesuada ultrices. Sed id varius urna. Nulla facilisi. Integer ut dui id neque consequat mollis. Sed ac velit libero. Sed vitae magna velit. Vivamus posuere hendrerit lacinia. Sed efficitur eleifend ante, in fermentum libero tempus sed. Curabitur vitae est quis neque sollicitudin consequat eget ac nunc.</p>
+            </div>
+        </div>
     )
 }
 
--- ieee140-r10-frontend/components/LandingPage/Carousal.jsx
+++ ieee140-r10-frontend/components/LandingPage/Carousal.jsx
@@ -1,8 +1,8 @@
 const Carousal = () => {
     return(
-        <>
-            
-        </>
+        <div style={{height:"100vh",width:"100vw"}}>
+            Carousal
+        </div>
     )
 }
 
