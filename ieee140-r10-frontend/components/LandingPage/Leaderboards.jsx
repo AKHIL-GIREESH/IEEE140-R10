8bbc3f58ea8203c969d056701ec7df1bbd659d0d
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-02-28 14:46:25 +0530

    Landing Pg

--- ieee140-r10-frontend/components/LandingPage/Leaderboards.jsx
+++ ieee140-r10-frontend/components/LandingPage/Leaderboards.jsx
@@ -1,8 +1,8 @@
 const Leaderboards = () => {
     return(
-        <>
-            
-        </>
+        <div style={{height:"150vh",width:"90vw",border:"1px solid",marginLeft:"5vw"}}>
+            Leaderboards
+        </div>
     )
 }
 
--- ieee140-r10-frontend/components/LandingPage/footer.jsx
+++ ieee140-r10-frontend/components/LandingPage/footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
     return(
-        <>
+        <div style={{height:"35vh",width:"90vw",border:"1px solid",marginLeft:"5vw",backgroundColor:"#0073DD"}}>
             Footer
-        </>
+        </div>
     )
 }
 
