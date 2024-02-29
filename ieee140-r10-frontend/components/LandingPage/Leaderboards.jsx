9179da764c0a74bd51ea96567a6a08131c7b990f
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-02-29 05:38:51 +0530

    Landing Leaderboards

--- ieee140-r10-frontend/components/LandingPage/Leaderboards.jsx
+++ ieee140-r10-frontend/components/LandingPage/Leaderboards.jsx
@@ -1,26 +1,28 @@
+import './LandingPg.css'
+
 const Leaderboards = () => {
 
     let table = []
 
     for(let i=0;i<10;i++){
         table.push(
-            <div style={{display:"flex",borderBottom:"1px solid #c9c9c9"}}>
-                <p>Team {i}</p> <p style={{marginLeft:"20vw"}}>{90+i}</p>
+            <div style={{display:"flex",width:"60vw",borderBottom:"1px solid #c9c9c9",justifyContent:"space-evenly"}}>
+                <p>Team {6+i}</p> <p>{90+i}</p>
             </div>
         )
     }
 
     return(
-        <div style={{height:"150vh",width:"90vw",border:"1px solid",marginLeft:"5vw"}}>
+        <div style={{height:"150vh",width:"90vw",marginLeft:"5vw"}}>
             <h1>Leaderboards</h1>
-            <div style={{border:"1px solid"}}>
+            <div className="landingPgButtons">
                 <button>INDIVIDUAL</button>
-                <button>TEAM</button>
+                <button style={{margin:"0 2.5vw"}}>TEAM</button>
                 <button>SECTION</button>
             </div>
-            <div style={{height:"100vh",border:"1px solid"}}>
-                <div style={{display:"flex",borderBottom:"1px solid #c9c9c9"}}>
-                    <p>Team Name</p> <p style={{marginLeft:"20vw"}}>100</p>
+            <div style={{display:"flex",height:"120vh",flexDirection:"column",justifyContent:"space-between",alignItems:"center"}}>
+                <div style={{display:"flex",width:"60vw",borderBottom:"1px solid #c9c9c9",marginTop:"8vh",justifyContent:"space-evenly"}}>
+                    <p>Team Name</p> <p>Points</p>
                 </div>
                 { 
                     table
