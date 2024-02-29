144f92eade5d898da199985b2aa7a0393e2c297f
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-02-29 23:12:09 +0530

    LeaderBoards

--- ieee140-r10-frontend/components/LandingPage/Leaderboards.jsx
+++ ieee140-r10-frontend/components/LandingPage/Leaderboards.jsx
@@ -1,6 +1,12 @@
+import { useState } from 'react'
 import './LandingPg.css'
 
 const Leaderboards = () => {
+    const [current,setCurrent] = useState("TEAM")
+    const highlightedStyle = {
+        color:"#0073DD",
+        backgroundColor:"white"
+    }
 
     let table = []
 
@@ -16,9 +22,9 @@ const Leaderboards = () => {
         <div style={{height:"150vh",width:"90vw",marginLeft:"5vw"}}>
             <h1>Leaderboards</h1>
             <div className="landingPgButtons">
-                <button>INDIVIDUAL</button>
-                <button style={{margin:"0 2.5vw"}}>TEAM</button>
-                <button>SECTION</button>
+                <button style={current === "INDIVIDUAL"?highlightedStyle:null} onClick={() => setCurrent("INDIVIDUAL")}>INDIVIDUAL</button>
+                <button style={current === "TEAM"?{...highlightedStyle,margin:"0 2.5vw"}:{margin:"0 2.5vw"}} onClick={() => setCurrent("TEAM")}>TEAM</button>
+                <button style={current === "SECTION"?highlightedStyle:null} onClick={() => setCurrent("SECTION")}>SECTION</button>
             </div>
             <div style={{display:"flex",height:"120vh",flexDirection:"column",justifyContent:"space-between",alignItems:"center"}}>
                 <div style={{display:"flex",width:"60vw",borderBottom:"1px solid #c9c9c9",marginTop:"8vh",justifyContent:"space-evenly"}}>
