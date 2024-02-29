89074eab9db7d2b73146cd5912da231e31a763b1
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-02-29 14:04:07 +0530

    Img slider

--- ieee140-r10-frontend/components/LandingPage/Carousal.jsx
+++ ieee140-r10-frontend/components/LandingPage/Carousal.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react"
+
 const Carousal = () => {
+
+    const [image,setImage] = useState(0)
+
+    let imgList = []
+    for(let i=0;i<5;i++){
+        imgList.push(
+        <div style={{height:"70vh",minWidth:"100%",backgroundColor:"black"}} key={i}>
+            Div {i}
+        </div>)
+    }
+
+
     return(
         <div style={{height:"100vh",width:"100vw"}}>
-            Carousal
+            <div style={{display:"flex",height:"70vh",width:"80vw",marginLeft:"10vw",overflow:"hidden"}}>
+                {imgList}
+            </div>
         </div>
     )
 }
--- ieee140-r10-frontend/components/LandingPage/footer.jsx
+++ ieee140-r10-frontend/components/LandingPage/footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return(
         <div style={{height:"35vh",width:"90vw",border:"1px solid",marginLeft:"5vw",backgroundColor:"#0073DD"}}>
-            Footer
+           <p>{`THIS IS AN OFFICIAL IEEE REGION 10 (ASIA-PACIFIC REGION) WEBSITE.© COPYRIGHT 2023 IEEE - ALL RIGHTS RESERVED. USE OF THIS WEBSITE SIGNIFIES YOUR AGREEMENT TO THE IEEE TERMS AND CONDITIONS.`}</p>
         </div>
     )
 }
