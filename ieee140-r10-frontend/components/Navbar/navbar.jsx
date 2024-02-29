13033a1ce801620dba8a953d4950efc559816bed
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-02-29 22:57:20 +0530

    SignUpChoice

--- ieee140-r10-frontend/components/Navbar/navbar.jsx
+++ ieee140-r10-frontend/components/Navbar/navbar.jsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom"
+
 const Navbar = () => {
     return(
         <div style={{position:"sticky",top:10,width:"96vw",height:"15vh",marginLeft:"2vw",backgroundColor:"black",borderRadius:"10px",zIndex:3}}>
-            About
+            <Link to="/SignUpChoice">SignUp</Link>
         </div>
     )
 }
--- ieee140-r10-frontend/components/SignUp/SignUpChoice.jsx
+++ ieee140-r10-frontend/components/SignUp/SignUpChoice.jsx
@@ -1,19 +1,34 @@
+import { useState } from "react";
 import { MdRadioButtonChecked } from "react-icons/md"; //ticked
 import { MdRadioButtonUnchecked } from "react-icons/md";
 
 const SignUpChoice = () => {
+
+    const [radio,setRadio] = useState("IEEE")
+
+    let rbData = [
+        "IEEE",
+        "Non-IEEE"
+    ]
+
     return(
         <div className="fullScreenDivContainer" style={{height:"100vh",display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}}>
             <h1>Choose your Category</h1>
             <div style={{fontSize:"21px"}}>
-                <div>
+                {rbData.map(item => {
+                    return(<div style={{cursor:"pointer"}} onClick={() => setRadio(item)}>
+                        {item === radio?<MdRadioButtonChecked style={{color:"#0073DD"}}/>:<MdRadioButtonUnchecked/>}
+                        <span>{item}</span>
+                    </div>)
+                })}
+                {/* <div style={{cursor:"pointer"}} onClick={}>
                     <MdRadioButtonChecked style={{color:"#0073DD"}}/>
                     <span>IEEE</span>
                 </div>
-                <div>
+                <div style={{cursor:"pointer"}}>
                     <MdRadioButtonUnchecked/>
                     <span>Non-IEEE</span><br/>
-                </div>
+                </div> */}
             </div>
             <br/>
             <div>
