deef5b095ebd8f11e99dc5bc5e3e8af00b98ada0
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-02-29 23:26:24 +0530

    Navbar

--- ieee140-r10-frontend/components/Navbar/navbar.jsx
+++ ieee140-r10-frontend/components/Navbar/navbar.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom"
 
 const Navbar = () => {
     return(
-        <div style={{position:"sticky",top:10,width:"96vw",height:"15vh",marginLeft:"2vw",backgroundColor:"black",borderRadius:"10px",zIndex:3}}>
-            <Link to="/SignUpChoice">SignUp</Link>
+        <div style={{position:"sticky",top:10,width:"96vw",height:"15vh",marginLeft:"2vw",border:"1px solid",backgroundColor:"rgba(255,255,255,0.8)",borderRadius:"10px",zIndex:3}}>
+            <Link to="/SignUpChoice" style={{color:"black"}}>SignUp</Link>
         </div>
     )
 }
