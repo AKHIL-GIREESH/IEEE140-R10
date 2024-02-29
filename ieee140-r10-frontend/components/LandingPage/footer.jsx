07cd549b313af057c2d21229d5103baba14a2fc8
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-02-29 22:11:29 +0530

    Img slider

--- ieee140-r10-frontend/components/LandingPage/footer.jsx
+++ ieee140-r10-frontend/components/LandingPage/footer.jsx
@@ -1,7 +1,10 @@
 const Footer = () => {
     return(
         <div style={{height:"35vh",width:"100%",borderTop:"0.5px solid",backgroundColor:"#0073DD"}}>
-           <p>{`THIS IS AN OFFICIAL IEEE REGION 10 (ASIA-PACIFIC REGION) WEBSITE.© COPYRIGHT 2023 IEEE - ALL RIGHTS RESERVED. USE OF THIS WEBSITE SIGNIFIES YOUR AGREEMENT TO THE IEEE TERMS AND CONDITIONS.`}</p>
+            <div style={{marginLeft:"2%",width:"96%",display:"flex"}}><p style={{fontSize:"1.2rem",fontWeight:"600",width:"60%"}}>HOME  |  SITEMAP/MORE SITES  |  CONTACT  |  ACCESSIBILITY  |  NON-DISCRIMINATION POLICY  |  IEEE ETHICS REPORTING  |  IEEE PRIVACY POLICY  |  TERMS</p>
+            <p>Social media</p>
+            </div>
+            <p style={{textAlign:"center",marginLeft:"20%",marginTop:"13vh",fontWeight:"300",width:"60%",fontSize:"0.9rem"}}>{`THIS IS AN OFFICIAL IEEE REGION 10 (ASIA-PACIFIC REGION) WEBSITE.© COPYRIGHT 2023 IEEE - ALL RIGHTS RESERVED. USE OF THIS WEBSITE SIGNIFIES YOUR AGREEMENT TO THE IEEE TERMS AND CONDITIONS.`}</p>
         </div>
     )
 }
