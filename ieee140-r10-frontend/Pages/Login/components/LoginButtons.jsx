9ddf2c4bf12f8a7c69e9616ee4c69ee61ad4215d
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-03-28 23:56:46 +0530

    Login done

--- ieee140-r10-frontend/Pages/Login/components/LoginButtons.jsx
+++ ieee140-r10-frontend/Pages/Login/components/LoginButtons.jsx
@@ -1,20 +1,47 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import useLogin from "../hooks/useLogin"
+import { useMutation } from "@tanstack/react-query";
 
 const LoginButtons = ({loginData,setAuth,verifyMail,setHandleErr}) => {
 
-    const trig = useLogin({loginData,setAuth,setHandleErr})
+    // const trig = useLogin({loginData,setAuth,setHandleErr})
     
-    const loginFunc = async () => {
-        console.log("login")
-        const loggedIn = await trig.mutateAsync()
-        console.log(loggedIn?.data)
-    }
+    // const loginFunc = async () => {
+    //     console.log("login")
+    //     const loggedIn = await trig.mutateAsync()
+    //     console.log(loggedIn?.data)
+    // }
+    const navi = useNavigate()
+
+    const loggedUser = useMutation({
+        mutationFn: () =>
+          fetch("http://159.65.7.52:5000/api/auth/login/", {
+            method: "POST",
+            body: JSON.stringify(loginData),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          })
+            .then((resp) => resp.json())
+            .then((respJSON) => respJSON.data)
+            .catch((err) => console.log(err)),
+        onSuccess: (data) => {
+            setAuth({ user: data?.user.firstName, token: "Bearer " + data?.token })
+            navi('/')
+        },
+        onError: (error) => console.error("Error during sign-up:", error),
+      });
+
+      const loginFunc = () => {
+        loggedUser.mutate()
+      }
+    
     
     return(
         <div>
-            <button style={{color:"white"}} onClick={loginFunc} disabled={Object.values(loginData).includes("") && !verifyMail}>LOGIN</button>
+            <button style={{color:"white"}} onClick={loginFunc} disabled={Object.values(loginData).includes("") && !verifyMail}>{loggedUser.isPending?"Loading":"LOGIN"}</button>
             <button><Link to="/SignUpChoice" style={{textDecoration:"none",color:"white"}}>SIGNUP</Link></button>
+            {loggedUser.isError && <p>Oops Wrong Credentials!</p>}
         </div>
     )
 }
