121dc0309ac8ffafc6d5424d6aa204b689a93843
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-03-30 08:42:29 +0530

    SignUp choice styling

--- ieee140-r10-frontend/Pages/SignUp/SignUpChoice.jsx
+++ ieee140-r10-frontend/Pages/SignUp/SignUpChoice.jsx
@@ -8,20 +8,20 @@ const SignUpChoice = () => {
     const [radio,setRadio] = useState("IEEE")
 
     let rbData = [
-        "IEEE",
-        "Non-IEEE"
+        "IEEE Member",
+        "Non Member/Others"
     ]
 
     localStorage.setItem('MemType', radio)
 
     return(
         <div className="fullScreenDivContainer" style={{height:"100vh",display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}}>
-            <h1>Choose your Category</h1>
+            <p style={{fontSize:"2.5rem",fontWeight:"700"}}>CHOOSE YOUR <span style={{color:"#0073DD"}}>CATEGORY</span></p>
             <div style={{fontSize:"21px"}}>
                 {rbData.map(item => {
-                    return(<div style={{cursor:"pointer"}} onClick={() => setRadio(item)}>
-                        {item === radio?<MdRadioButtonChecked style={{color:"#0073DD"}}/>:<MdRadioButtonUnchecked/>}
-                        <span>{item}</span>
+                    return(<div style={{cursor:"pointer",display:"flex",margin:"3vh",alignItems:"center"}} onClick={() => setRadio(item)}>
+                        {item === radio?<MdRadioButtonChecked style={{color:"#0073DD",backgroundColor:"white",borderRadius:"50%"}}/>:<MdRadioButtonUnchecked style={{backgroundColor:"white",borderRadius:"50%"}}/>}
+                        <span style={{marginLeft:"15px"}}>{item}</span>
                     </div>)
                 })}
                 {/* <div style={{cursor:"pointer"}} onClick={}>
