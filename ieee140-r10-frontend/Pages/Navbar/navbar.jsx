6edfc5770fc14bbd3f5a0c2bf92cc87c4af913b6
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-04-04 11:39:40 +0530

    Navbar

--- ieee140-r10-frontend/Pages/Navbar/navbar.jsx
+++ ieee140-r10-frontend/Pages/Navbar/navbar.jsx
@@ -1,14 +1,27 @@
-import { Link } from "react-router-dom"
-import { useAuth } from "../../src/AuthProvider"
+import { Link } from "react-router-dom";
+import { useAuth } from "../../src/AuthProvider";
 
 const Navbar = () => {
+  const user = useAuth();
+  return (
+    <div
+      style={{
+        position: "sticky",
+        top: 10,
+        width: "100vw",
+        height: "8vh",
+        border: "1px solid",
+        backgroundColor: "rgba(255,255,255,0.8)",
+        zIndex: 3,
+      }}
+    >
+      <Link to="/FAQ">FAQ</Link>
+      <a href="#">Leaderboards</a>
+      <Link to="/Login" style={{ color: "black" }}>
+        {user === null ? "Login" : user}
+      </Link>
+    </div>
+  );
+};
 
-    const user = useAuth()
-    return(
-        <div style={{position:"sticky",top:10,width:"96vw",height:"15vh",marginLeft:"2vw",border:"1px solid",backgroundColor:"rgba(255,255,255,0.8)",borderRadius:"10px",zIndex:3}}>
-            <Link to="/Login" style={{color:"black"}}>{user === null?"Sign Up":user}</Link>
-        </div>
-    )
-}
-
-export default Navbar
+export default Navbar;
