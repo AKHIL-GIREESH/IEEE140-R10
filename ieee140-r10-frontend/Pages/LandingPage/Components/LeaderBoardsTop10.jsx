7ae3557d65bbe5305048bb04d50349e25cb66655
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-04-04 15:29:20 +0530

    Leaderboards UI

--- ieee140-r10-frontend/Pages/LandingPage/Components/LeaderBoardsTop10.jsx
+++ ieee140-r10-frontend/Pages/LandingPage/Components/LeaderBoardsTop10.jsx
@@ -1,45 +1,54 @@
-import { useQuery } from "@tanstack/react-query"
+import { useQuery } from "@tanstack/react-query";
 
-const LeaderboardsTop10 = ({current}) => {
+const LeaderboardsTop10 = ({ current }) => {
+  const route = {
+    INDIVIDUAL: "individual",
+    TEAM: "teams",
+    SECTION: "section",
+  };
 
-    const route = {
-        "INDIVIDUAL":"individual",
-        "TEAM":"teams",
-        "SECTION":"section"
-    }
+  console.log(route[current]);
 
-    console.log(route[current])
+  const Data = useQuery({
+    queryFn: () =>
+      fetch(`http://159.65.7.52:5000/api/leaderboard/${route[current]}`).then(
+        (resp) => resp.json()
+      ),
+    queryKey: ["leaderBoards"],
+  });
 
+  //Data.isSuccess && queryClient.invalidateQueries(['leaderBoards'])
+  Data.isSuccess && console.log(Data.data);
+  // let table = []
 
-    const Data = useQuery({
-        queryFn:() => fetch(`http://159.65.7.52:5000/api/leaderboard/${route[current]}`).then(resp => resp.json()),
-        queryKey:["leaderBoards"]
-    })
+  // for(let i=0;i<10;i++){
+  //     table.push(
+  //         <div style={{display:"flex",width:"60vw",borderBottom:"1px solid #c9c9c9",justifyContent:"space-evenly"}}>
+  //             <p>Team {6+i}</p> <p>{90+i}</p>
+  //         </div>
+  //     )
+  // }
 
-    //Data.isSuccess && queryClient.invalidateQueries(['leaderBoards'])
-    Data.isSuccess && console.log(Data.data)
-    // let table = []
+  // console.log(Data?.data)
 
-    // for(let i=0;i<10;i++){
-    //     table.push(
-    //         <div style={{display:"flex",width:"60vw",borderBottom:"1px solid #c9c9c9",justifyContent:"space-evenly"}}>
-    //             <p>Team {6+i}</p> <p>{90+i}</p>
-    //         </div>
-    //     )
-    // }
-
-    // console.log(Data?.data)
-
-    return(
-        <div style={{display:"flex",height:"120vh",flexDirection:"column",justifyContent:"space-between",alignItems:"center"}}>
-            <div style={{display:"flex",width:"60vw",borderBottom:"1px solid #c9c9c9",marginTop:"8vh",justifyContent:"space-evenly"}}>
+  return (
+    <div
+      style={{
+        display: "flex",
+        height: "120vh",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      {/* <div style={{display:"flex",width:"60vw",borderBottom:"1px solid #c9c9c9",marginTop:"8vh",justifyContent:"space-evenly"}}>
                 <p>Team Name</p> <p>Points</p>
-            </div>
-            {Data.isLoading && <p>Loading</p>}
-            {Data.isError && <p>Error</p>}
-            {/* {Data.isSuccess && <p>{Data.data?.team[0].name}</p>} */}
-        </div>
-    )
-}
+            </div> */}
+      {Data.isLoading && <p>Loading</p>}
+      {Data.isError && <p>Error</p>}
+      {/* {Data.isSuccess && <div>{Data.data?.team[0].name}</div>} */}
+    </div>
+  );
+};
 
-export default LeaderboardsTop10
+export default LeaderboardsTop10;
