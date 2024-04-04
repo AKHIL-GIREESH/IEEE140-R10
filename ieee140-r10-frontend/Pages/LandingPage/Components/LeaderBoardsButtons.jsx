1f5196afcbbb68a6b59717fa3e5ef3ba4ccfbfe8
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-04-04 14:11:45 +0530

    Leaderboards UI

--- ieee140-r10-frontend/Pages/LandingPage/Components/LeaderBoardsButtons.jsx
+++ ieee140-r10-frontend/Pages/LandingPage/Components/LeaderBoardsButtons.jsx
@@ -1,30 +1,48 @@
-import { useQueryClient } from '@tanstack/react-query'
+import { useQueryClient } from "@tanstack/react-query";
 
-const LeaderboardsButtons = ({setCurrent,current}) => {
+const LeaderboardsButtons = ({ setCurrent, current }) => {
+  const highlightedStyle = {
+    color: "#0073DD",
+    backgroundColor: "white",
+  };
+  //const queryClient = useQueryClient()
+  //queryClient.invalidateQueries(['leaderBoards'])
 
-    const highlightedStyle = {
-        color:"#0073DD",
-        backgroundColor:"white"
-    }
-    //const queryClient = useQueryClient()
-    //queryClient.invalidateQueries(['leaderBoards'])
+  return (
+    <div className="landingPgButtons">
+      <button
+        style={current === "INDIVIDUAL" ? highlightedStyle : null}
+        onClick={() => {
+          setCurrent("INDIVIDUAL");
+          //queryClient.invalidateQueries(['leaderBoards'])
+        }}
+      >
+        INDIVIDUAL
+      </button>
+      <button
+        style={
+          current === "TEAM"
+            ? { ...highlightedStyle, margin: "0 2.5vw" }
+            : { margin: "0 2.5vw" }
+        }
+        onClick={() => {
+          setCurrent("TEAM");
+          //queryClient.invalidateQueries(['leaderBoards'])
+        }}
+      >
+        TEAM
+      </button>
+      <button
+        style={current === "SECTION" ? highlightedStyle : null}
+        onClick={() => {
+          setCurrent("SECTION");
+          //queryClient.invalidateQueries(['leaderBoards'])
+        }}
+      >
+        SECTION
+      </button>
+    </div>
+  );
+};
 
-    return (
-        <div className="landingPgButtons">
-            <button style={current === "INDIVIDUAL"?highlightedStyle:null} onClick={() => {
-                setCurrent("INDIVIDUAL")
-                //queryClient.invalidateQueries(['leaderBoards'])
-            }}>INDIVIDUAL</button>
-            <button style={current === "TEAM"?{...highlightedStyle,margin:"0 2.5vw"}:{margin:"0 2.5vw"}} onClick={() => {
-                setCurrent("TEAM")
-                //queryClient.invalidateQueries(['leaderBoards'])
-            }}>TEAM</button>
-            <button style={current === "SECTION"?highlightedStyle:null} onClick={() => {
-                setCurrent("SECTION")
-                //queryClient.invalidateQueries(['leaderBoards'])
-            }}>SECTION</button>
-        </div>
-    )
-}   
-
-export default LeaderboardsButtons
+export default LeaderboardsButtons;
