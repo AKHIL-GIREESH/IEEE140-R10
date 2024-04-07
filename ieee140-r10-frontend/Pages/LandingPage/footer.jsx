3558e9e7bbc211cea7fbd1a79dc2fef52770d43f
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-04-08 02:03:40 +0530

    Theme

--- ieee140-r10-frontend/Pages/LandingPage/footer.jsx
+++ ieee140-r10-frontend/Pages/LandingPage/footer.jsx
@@ -1,12 +1,50 @@
-const Footer = () => {
-    return(
-        <div style={{height:"35vh",width:"100%",borderTop:"0.5px solid",backgroundColor:"#0073DD"}}>
-            <div style={{marginLeft:"2%",width:"96%",display:"flex"}}><p style={{fontSize:"1.2rem",fontWeight:"600",width:"60%"}}>HOME  |  SITEMAP/MORE SITES  |  CONTACT  |  ACCESSIBILITY  |  NON-DISCRIMINATION POLICY  |  IEEE ETHICS REPORTING  |  IEEE PRIVACY POLICY  |  TERMS</p>
-            <p>Social media</p>
-            </div>
-            <p style={{textAlign:"center",marginLeft:"20%",marginTop:"13vh",fontWeight:"300",width:"60%",fontSize:"0.9rem"}}>{`THIS IS AN OFFICIAL IEEE REGION 10 (ASIA-PACIFIC REGION) WEBSITE.© COPYRIGHT 2023 IEEE - ALL RIGHTS RESERVED. USE OF THIS WEBSITE SIGNIFIES YOUR AGREEMENT TO THE IEEE TERMS AND CONDITIONS.`}</p>
-        </div>
-    )
-}
+import {
+  AiFillFacebook,
+  AiFillYoutube,
+  AiFillInstagram,
+  AiFillLinkedin,
+} from "react-icons/ai";
+import { FaSquareXTwitter } from "react-icons/fa6";
 
-export default Footer
+const Footer = () => {
+  return (
+    <div
+      style={{
+        height: "35vh",
+        width: "100%",
+        borderTop: "0.5px solid",
+        backgroundColor: "#0073DD",
+      }}
+    >
+      <div style={{ marginLeft: "2%", width: "96%", display: "flex" }}>
+        <p style={{ fontSize: "1.2rem", fontWeight: "600", width: "60%" }}>
+          HOME | SITEMAP/MORE SITES | CONTACT | ACCESSIBILITY |
+          NON-DISCRIMINATION POLICY | IEEE ETHICS REPORTING | IEEE PRIVACY
+          POLICY | TERMS
+        </p>
+        <div>
+          <p>FOLLOW US</p>
+          <div>
+            <AiFillFacebook />
+            <FaSquareXTwitter />
+            <AiFillInstagram />
+            <AiFillYoutube />
+            <AiFillLinkedin />
+          </div>
+        </div>
+      </div>
+      <p
+        style={{
+          textAlign: "center",
+          marginLeft: "20%",
+          marginTop: "13vh",
+          fontWeight: "300",
+          width: "60%",
+          fontSize: "0.9rem",
+        }}
+      >{`THIS IS AN OFFICIAL IEEE REGION 10 (ASIA-PACIFIC REGION) WEBSITE.© COPYRIGHT 2023 IEEE - ALL RIGHTS RESERVED. USE OF THIS WEBSITE SIGNIFIES YOUR AGREEMENT TO THE IEEE TERMS AND CONDITIONS.`}</p>
+    </div>
+  );
+};
+
+export default Footer;
--- ieee140-r10-frontend/src/main.jsx
+++ ieee140-r10-frontend/src/main.jsx
@@ -1,53 +1,59 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import LandingPage from '../Pages/LandingPage/Landing.jsx'
-import SignUp from '../Pages/SignUp/SignUp.jsx'
-import SignUpChoice from '../Pages/SignUp/SignUpChoice.jsx'
-import Login from '../Pages/Login/Login.jsx'
-import Profile from '../Pages/Profile/Profile'
-import FAQ from '../Pages/FAQ/FAQ.jsx'
-import AuthProvider from './AuthProvider.jsx'
-import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query'
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LandingPage from "../Pages/LandingPage/Landing.jsx";
+import SignUp from "../Pages/SignUp/SignUp.jsx";
+import SignUpChoice from "../Pages/SignUp/SignUpChoice.jsx";
+import Login from "../Pages/Login/Login.jsx";
+import Profile from "../Pages/Profile/Profile";
+import FAQ from "../Pages/FAQ/FAQ.jsx";
+import AuthProvider from "./AuthProvider.jsx";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
+import Theme from "./Theme";
+import ThemeProvider from "./Theme";
 
 const router = createBrowserRouter([
   {
-    path:"/",
-    element:<LandingPage/>
+    path: "/",
+    element: <LandingPage />,
   },
   {
-    path:"/signUp",
-    element:<SignUp/>
+    path: "/signUp",
+    element: <SignUp />,
   },
   {
-    path:"/signUpChoice",
-    element:<SignUpChoice/>
+    path: "/signUpChoice",
+    element: <SignUpChoice />,
   },
   {
-    path:"/Login",
-    element:<Login/>
+    path: "/Login",
+    element: <Login />,
   },
   {
-    path:"/Profile",
-    element:<Profile/>
+    path: "/Profile",
+    element: <Profile />,
   },
   {
-    path:"/FAQ",
-    element:<FAQ/>
-  }
-])
+    path: "/FAQ",
+    element: <FAQ />,
+  },
+]);
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
         <AuthProvider>
           <RouterProvider router={router} />
         </AuthProvider>
-      </QueryClientProvider>
-  </React.StrictMode>,
-)
+      </ThemeProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
