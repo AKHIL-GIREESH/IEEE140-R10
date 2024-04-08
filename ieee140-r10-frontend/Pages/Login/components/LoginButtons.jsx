d8835458ba30f6a41ae56924380b3cf03393db80
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-04-08 08:47:58 +0530

    Theme index

--- ieee140-r10-frontend/Pages/Login/components/LoginButtons.jsx
+++ ieee140-r10-frontend/Pages/Login/components/LoginButtons.jsx
@@ -1,63 +1,91 @@
-import { Link, useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom";
 import { useMutation } from "@tanstack/react-query";
 import ReCAPTCHA from "react-google-recaptcha";
 import { useState } from "react";
+import { useTheme } from "../../../src/Theme";
 
-const LoginButtons = ({loginData,setAuth,verifyMail}) => {
+const LoginButtons = ({ loginData, setAuth, verifyMail }) => {
+  // const trig = useLogin({loginData,setAuth,setHandleErr})
+  const { theme } = useTheme();
+  // const loginFunc = async () => {
+  //     console.log("login")
+  //     const loggedIn = await trig.mutateAsync()
+  //     console.log(loggedIn?.data)
+  // }
 
-    // const trig = useLogin({loginData,setAuth,setHandleErr})
-    
-    // const loginFunc = async () => {
-    //     console.log("login")
-    //     const loggedIn = await trig.mutateAsync()
-    //     console.log(loggedIn?.data)
-    // }
+  const [verifyCaptcha, setVerifyCaptcha] = useState(false);
 
-    const [verifyCaptcha,setVerifyCaptcha] = useState(false)
+  const onChange = () => {
+    setVerifyCaptcha(true);
+    console.log(verifyCaptcha);
+  };
 
-    const onChange = () => {
-     setVerifyCaptcha(true)
-      console.log(verifyCaptcha)
-    }
+  const navi = useNavigate();
 
-    const navi = useNavigate()
-
-    const loggedUser = useMutation({
-        mutationFn: () =>
-          fetch("http://159.65.7.52:5000/api/auth/login/", {
-            method: "POST",
-            body: JSON.stringify(loginData),
-            headers: {
-              "Content-Type": "application/json",
-            },
-          })
-            .then((resp) => resp.json())
-            .then((respJSON) => respJSON.data)
-            .catch((err) => console.log(err)),
-        onSuccess: (data) => {
-            setAuth({ user: data?.user.firstName, token: "Bearer " + data?.token })
-            navi('/',{replace:true})
+  const loggedUser = useMutation({
+    mutationFn: () =>
+      fetch("http://159.65.7.52:5000/api/auth/login/", {
+        method: "POST",
+        body: JSON.stringify(loginData),
+        headers: {
+          "Content-Type": "application/json",
         },
-        onError: (error) => console.error("Error during sign-up:", error),
-      });
+      })
+        .then((resp) => resp.json())
+        .then((respJSON) => respJSON.data)
+        .catch((err) => console.log(err)),
+    onSuccess: (data) => {
+      setAuth({ user: data?.user.firstName, token: "Bearer " + data?.token });
+      navi("/", { replace: true });
+    },
+    onError: (error) => console.error("Error during sign-up:", error),
+  });
 
-      const loginFunc = () => {
-        loggedUser.mutate()
-      }
-    
-      console.log(verifyCaptcha)
-    return(
-        <div style={{height:"60%",display:"flex",flexDirection:"column",justifyContent:"space-between"}}>
-            <ReCAPTCHA
-                sitekey="6Leqq6gpAAAAAL617ua6e5nwhGkvEFkT_cAEv4dP"
-                onChange={onChange}
-                theme="dark light"
-            />
-            <button className="DisabledButton" onClick={loginFunc} disabled={Object.values(loginData).includes("") || !verifyMail || !verifyCaptcha || loginData.password.length < 1}>{loggedUser.isPending?"Loading":"LOGIN"}</button>
-            {loggedUser.isError && <p>Oops Wrong Credentials!</p>}
-            <p style={{alignSelf:"center"}}><Link to="/SignUpChoice" style={{textDecoration:"none",color:"white"}}>Create an Account →</Link></p>
-        </div>
-    )
-}
+  const loginFunc = () => {
+    loggedUser.mutate();
+  };
 
-export default LoginButtons
+  console.log(verifyCaptcha);
+  return (
+    <div
+      style={{
+        height: "60%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+      }}
+    >
+      <ReCAPTCHA
+        sitekey="6Leqq6gpAAAAAL617ua6e5nwhGkvEFkT_cAEv4dP"
+        onChange={onChange}
+        theme="dark light"
+      />
+      <button
+        className="DisabledButton"
+        onClick={loginFunc}
+        disabled={
+          Object.values(loginData).includes("") ||
+          !verifyMail ||
+          !verifyCaptcha ||
+          loginData.password.length < 1
+        }
+      >
+        {loggedUser.isPending ? "Loading" : "LOGIN"}
+      </button>
+      {loggedUser.isError && <p>Oops Wrong Credentials!</p>}
+      <p style={{ alignSelf: "center" }}>
+        <Link
+          to="/SignUpChoice"
+          style={{
+            textDecoration: "none",
+            color: theme === "light" ? "black" : "white",
+          }}
+        >
+          Create an Account →
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default LoginButtons;
--- ieee140-r10-frontend/src/Theme.jsx
+++ ieee140-r10-frontend/src/Theme.jsx
@@ -1,4 +1,4 @@
-import { useState, createContext, useContext } from "react";
+import { useState, createContext, useContext, useEffect } from "react";
 
 const ThemeContext = createContext(null);
 
@@ -6,6 +6,16 @@ export const useTheme = () => useContext(ThemeContext);
 
 export default function ThemeProvider({ children }) {
   const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    if (theme === "light") {
+      document.documentElement.style.backgroundColor = "#EDEDED";
+      document.documentElement.style.color = "#000000";
+    } else {
+      document.documentElement.style.backgroundColor = "#1E1E1E";
+      document.documentElement.style.color = "rgba(255, 255, 255, 0.87)";
+    }
+  }, [theme]);
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
