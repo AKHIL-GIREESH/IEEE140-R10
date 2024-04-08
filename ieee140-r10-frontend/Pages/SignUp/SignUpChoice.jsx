081d78f3571fa5762ca67e27a0abe19f00c44b44
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-04-08 06:18:07 +0530

    Theme SignUp

--- ieee140-r10-frontend/Pages/SignUp/SignUpChoice.jsx
+++ ieee140-r10-frontend/Pages/SignUp/SignUpChoice.jsx
@@ -2,29 +2,70 @@ import { useState } from "react";
 import { MdRadioButtonChecked } from "react-icons/md"; //ticked
 import { MdRadioButtonUnchecked } from "react-icons/md";
 import { Link } from "react-router-dom";
+import { useTheme } from "../../src/Theme";
 
 const SignUpChoice = () => {
+  const [radio, setRadio] = useState("IEEE Member");
 
-    const [radio,setRadio] = useState("IEEE Member")
+  let rbData = ["IEEE Member", "Non Member/Others"];
 
-    let rbData = [
-        "IEEE Member",
-        "Non Member/Others"
-    ]
+  localStorage.setItem("MemType", radio);
 
-    localStorage.setItem('MemType', radio)
+  const { theme } = useTheme();
 
-    return(
-        <div className="fullScreenDivContainer" style={{height:"100vh",display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}}>
-            <p style={{fontSize:window.innerHeight>window.innerWidth?"1.7rem":"2.5rem",fontWeight:"700"}}>CHOOSE YOUR <span style={{color:"#0073DD"}}>CATEGORY</span></p>
-            <div style={{fontSize:"21px"}}>
-                {rbData.map(item => {
-                    return(<div style={{cursor:"pointer",display:"flex",margin:"3vh",alignItems:"center"}} onClick={() => setRadio(item)}>
-                        {item === radio?<MdRadioButtonChecked style={{color:"#0073DD",backgroundColor:"white",borderRadius:"50%"}}/>:<MdRadioButtonUnchecked style={{backgroundColor:"white",borderRadius:"50%"}}/>}
-                        <span style={{marginLeft:"15px"}}>{item}</span>
-                    </div>)
-                })}
-                {/* <div style={{cursor:"pointer"}} onClick={}>
+  return (
+    <div
+      className="fullScreenDivContainer"
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <p
+        style={{
+          fontSize:
+            window.innerHeight > window.innerWidth ? "1.7rem" : "2.5rem",
+          fontWeight: "700",
+        }}
+      >
+        CHOOSE YOUR{" "}
+        <span style={{ color: theme === "light" ? "#2C8ABF" : "#0073DD" }}>
+          CATEGORY
+        </span>
+      </p>
+      <div style={{ fontSize: "21px" }}>
+        {rbData.map((item) => {
+          return (
+            <div
+              style={{
+                cursor: "pointer",
+                display: "flex",
+                margin: "3vh",
+                alignItems: "center",
+              }}
+              onClick={() => setRadio(item)}
+            >
+              {item === radio ? (
+                <MdRadioButtonChecked
+                  style={{
+                    color: theme === "light" ? "#2C8ABF" : "#0073DD",
+                    backgroundColor: "white",
+                    borderRadius: "50%",
+                  }}
+                />
+              ) : (
+                <MdRadioButtonUnchecked
+                  style={{ backgroundColor: "white", borderRadius: "50%" }}
+                />
+              )}
+              <span style={{ marginLeft: "15px" }}>{item}</span>
+            </div>
+          );
+        })}
+        {/* <div style={{cursor:"pointer"}} onClick={}>
                     <MdRadioButtonChecked style={{color:"#0073DD"}}/>
                     <span>IEEE</span>
                 </div>
@@ -32,15 +73,38 @@ const SignUpChoice = () => {
                     <MdRadioButtonUnchecked/>
                     <span>Non-IEEE</span><br/>
                 </div> */}
-            </div>
-            <br/>
-            <div className="SignUpChoice">
-                <button style={window.innerHeight>window.innerWidth?{width:"20vw",height:"6vh"}:null}><Link to="/">BACK</Link></button>
-                <button style={window.innerHeight>window.innerWidth?{width:"20vw",height:"6vh"}:null}><Link to="/SignUp">NEXT</Link></button>
-            </div>
-            
-        </div>
-    )
-}
+      </div>
+      <br />
+      <div className="SignUpChoice">
+        <button
+          style={
+            window.innerHeight > window.innerWidth
+              ? {
+                  width: "20vw",
+                  height: "6vh",
+                  backgroundColor: theme === "light" ? "#2C8ABF" : "#0073DD",
+                }
+              : { backgroundColor: theme === "light" ? "#2C8ABF" : "#0073DD" }
+          }
+        >
+          <Link to="/">BACK</Link>
+        </button>
+        <button
+          style={
+            window.innerHeight > window.innerWidth
+              ? {
+                  width: "20vw",
+                  height: "6vh",
+                  backgroundColor: theme === "light" ? "#2C8ABF" : "#0073DD",
+                }
+              : { backgroundColor: theme === "light" ? "#2C8ABF" : "#0073DD" }
+          }
+        >
+          <Link to="/SignUp">NEXT</Link>
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default SignUpChoice
+export default SignUpChoice;
--- ieee140-r10-frontend/Pages/SignUp/components/SignUpButtons.jsx
+++ ieee140-r10-frontend/Pages/SignUp/components/SignUpButtons.jsx
@@ -1,66 +1,110 @@
-import { Link,useNavigate } from 'react-router-dom'
-import { useMutation } from "@tanstack/react-query"
-import ReCAPTCHA from 'react-google-recaptcha'
+import { Link, useNavigate } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
+import ReCAPTCHA from "react-google-recaptcha";
+import { useTheme } from "../../../src/Theme";
 
-const SignUpButtons = ({signUpData,verifyMail,setAuth,verifyPassword}) => {
+const SignUpButtons = ({ signUpData, verifyMail, setAuth, verifyPassword }) => {
+  // const data = useSignIn(signUpData)
+  // console.log(data?.data)
 
-    // const data = useSignIn(signUpData)
-    // console.log(data?.data)
+  // const SignUpFunc = () => {
+  //     console.log("login")
+  //     data.mutate()
+  // }
+  let verifyCaptcha = false;
+  const { theme } = useTheme();
 
-    // const SignUpFunc = () => {
-    //     console.log("login")
-    //     data.mutate()
-    // }
-    let verifyCaptcha = false
+  const onChange = () => {
+    verifyCaptcha = true;
+  };
 
-    const onChange = () => {
-        verifyCaptcha = true
-    }
+  const navi = useNavigate();
 
-    const navi = useNavigate()
+  const { confirmPassword, ...apiData } = signUpData; //delete signUpData.confirmPassword
 
-    const {confirmPassword,...apiData} = signUpData //delete signUpData.confirmPassword
-
-    const signedUser = useMutation({
-        mutationKey:["signUp"],
-        mutationFn:() => fetch("http://159.65.7.52:5000/api/auth/register/",{method:"POST",body: JSON.stringify(apiData),
+  const signedUser = useMutation({
+    mutationKey: ["signUp"],
+    mutationFn: () =>
+      fetch("http://159.65.7.52:5000/api/auth/register/", {
+        method: "POST",
+        body: JSON.stringify(apiData),
         headers: {
-          "Content-Type": "application/json"
-        }})
-        .then(resp => resp.json())
-        .then(respJSON => respJSON.data)
-        .catch(err => console.log(err)),
-        onSuccess: data => {
-            console.log("WOrks")
-            setAuth({user:data.user.firstName,token:"Bearer "+data.token})
-            navi('/',{replace:true})
+          "Content-Type": "application/json",
         },
-        onError: error => console.error('Error during sign-up:', error)
-        })
-    
-    const SignUpFunc = () => {
-        signedUser.mutate()
-    }
+      })
+        .then((resp) => resp.json())
+        .then((respJSON) => respJSON.data)
+        .catch((err) => console.log(err)),
+    onSuccess: (data) => {
+      console.log("WOrks");
+      setAuth({ user: data.user.firstName, token: "Bearer " + data.token });
+      navi("/", { replace: true });
+    },
+    onError: (error) => console.error("Error during sign-up:", error),
+  });
 
-    console.log(Object.values(signUpData).includes("") && !verifyMail && !verifyPassword) //&& !verifyCaptcha)
+  const SignUpFunc = () => {
+    signedUser.mutate();
+  };
 
-    return (
-        <div style={{height:"20vh",width:"100%",display:"flex",flexDirection:"column",justifyContent:"center"}}>
-            <div style={{height:"50%",alignSelf:"flex-start"}}>
-                <ReCAPTCHA 
-                    className='captcha'
-                    sitekey="6Leqq6gpAAAAAL617ua6e5nwhGkvEFkT_cAEv4dP"
-                    onChange={onChange}
-                />
-                {signedUser.isError && <p>Something went Wrong</p>}
-            </div>
-            <div style={{height:"50%",marginTop:"2%",width:window.innerHeight>window.innerWidth?"60vw":null}}>
-                <button><Link to="/SignUpChoice" style={{textDecoration:"none",color:"white"}}>BACK</Link></button> 
-                <button className="DisabledButton" style={{textDecoration:"none"}} onClick={SignUpFunc} disabled={Object.values(signUpData).includes("") && !verifyMail && !verifyPassword}>{signedUser.isPending?"Loading":"SIGNUP"}</button>
-            </div>
-            {/* disabled={Object.values(signUpData).includes("") && !verifyMail && !verifyPassword } */}
-        </div>
-    )
-}
+  console.log(
+    Object.values(signUpData).includes("") && !verifyMail && !verifyPassword
+  ); //&& !verifyCaptcha)
 
-export default SignUpButtons
+  return (
+    <div
+      style={{
+        height: "20vh",
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+      }}
+    >
+      <div style={{ height: "50%", alignSelf: "flex-start" }}>
+        <ReCAPTCHA
+          className="captcha"
+          sitekey="6Leqq6gpAAAAAL617ua6e5nwhGkvEFkT_cAEv4dP"
+          onChange={onChange}
+        />
+        {signedUser.isError && <p>Something went Wrong</p>}
+      </div>
+      <div
+        style={{
+          height: "50%",
+          marginTop: "2%",
+          width: window.innerHeight > window.innerWidth ? "60vw" : null,
+        }}
+      >
+        <button
+          style={{ backgroundColor: theme === "light" ? "#2C8ABF" : "#0073DD" }}
+        >
+          <Link
+            to="/SignUpChoice"
+            style={{
+              textDecoration: "none",
+              color: "white",
+            }}
+          >
+            BACK
+          </Link>
+        </button>
+        <button
+          className="DisabledButton"
+          style={{ textDecoration: "none" }}
+          onClick={SignUpFunc}
+          disabled={
+            Object.values(signUpData).includes("") &&
+            !verifyMail &&
+            !verifyPassword
+          }
+        >
+          {signedUser.isPending ? "Loading" : "SIGNUP"}
+        </button>
+      </div>
+      {/* disabled={Object.values(signUpData).includes("") && !verifyMail && !verifyPassword } */}
+    </div>
+  );
+};
+
+export default SignUpButtons;
